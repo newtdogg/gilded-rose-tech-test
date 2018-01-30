a780c154bef8187cb5a1beab54bf3dda30e3f58d
newtdogg <edwardfrancisnewton@live.com>
2018-01-30 14:20:41 +0000

    added first tests for gilded rose which now pass, currently not doubling item

--- spec/unit_tests.rb/gilded_rose_spec.rb
+++ spec/unit_tests.rb/gilded_rose_spec.rb
@@ -1,9 +1,22 @@
-# describe GildedRose do
-#
-  # describe "#update_quality" do
-  #   it "does not change the name" do
-  #
-  #   end
-  # end
-#
-# end
+require "gilded_rose"
+require "item"
+
+describe GildedRose do
+
+  items = [Item.new("Bread", 4, 10)]
+  subject(:gildedrose) { described_class.new(items)}
+
+  describe "initialize" do
+    it "initializes with an item" do
+      expect(gildedrose.items).to be_a(Array)
+    end
+  end
+
+  describe "update_quality" do
+    it "normal items degrade 1 point of quality per day" do
+      gildedrose.update_quality
+      expect(gildedrose.items[0].quality).to eq(9)
+    end
+  end
+
+end
