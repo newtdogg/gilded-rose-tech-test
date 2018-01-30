3ea67f17db83e16f61b3f878b3e619c7ccf6539c
newtdogg <edwardfrancisnewton@live.com>
2018-01-30 14:39:44 +0000

    added more basic tests for the update quality function, long winded tests require refactoring as well as doubles

--- spec/unit_tests.rb/gilded_rose_spec.rb
+++ spec/unit_tests.rb/gilded_rose_spec.rb
@@ -3,19 +3,34 @@ require "item"
 
 describe GildedRose do
 
-  items = [Item.new("Bread", 4, 10)]
-  subject(:gildedrose) { described_class.new(items)}
-
   describe "initialize" do
     it "initializes with an item" do
+      items = [Item.new("Bread", 4, 10)]
+      gildedrose = GildedRose.new(items)
       expect(gildedrose.items).to be_a(Array)
     end
   end
 
-  describe "update_quality" do
-    it "normal items degrade 1 point of quality per day" do
+  describe "update_quality for normal items" do
+    it "items degrade 1 point of quality per day" do
+      items = [Item.new("Bread", 4, 10)]
+      gildedrose = GildedRose.new(items)
       gildedrose.update_quality
       expect(gildedrose.items[0].quality).to eq(9)
+    end
+
+    it "items date to sell is reduced by one per day" do
+      items = [Item.new("Bread", 4, 10)]
+      gildedrose = GildedRose.new(items)
+      gildedrose.update_quality
+      expect(gildedrose.items[0].sell_in).to eq(3)
+    end
+
+    it "items should lose value twice as fast after their sell by date" do
+      items = [Item.new("Bread", 0, 10)]
+      gildedrose = GildedRose.new(items)
+      gildedrose.update_quality
+      expect(gildedrose.items[0].quality).to eq(8)
     end
   end
 
